147c8e22feb323b2c60715d39ecb8cd52757587b
Lim J <149343911+LimJ2023@users.noreply.github.com>
2024-09-01 02:40:51 +0900

    server 파일 소켓io 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
+import socket from "./server";
 
 function App() {
-  return (
-    <div>
-      <div className="App"></div>
-    </div>
-  );
+    return (
+        <div>
+            <div className="App"></div>
+        </div>
+    );
 }
 
 export default App;
